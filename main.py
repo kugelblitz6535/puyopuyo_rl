e48d9d3f19d24e9da3b3074269ce6b29c3655b64
kugelblitz6535 <user.email=kugelblitz1592@gmail.com>
2019-09-11 19:23:21 +0900

    今日の分

--- main.py
+++ main.py
@@ -1,22 +1,184 @@
-from gym.envs.registration import make
+from collections import deque
 
-from gym_puyopuyo.agent import TsuTreeSearchAgent
+import numpy as np
+from gym.envs.registration import make
 from gym_puyopuyo import register
+from gym_puyopuyo.agent import TsuTreeSearchAgent
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.optimizers import Adam
+from tensorflow.losses import huber_loss
+
+
+NUM_EPISODES = 500  # エピソード数
+MAX_STEPS = 2000  # 最大ステップ数
+GAMMA = 0.99  # 時間割引率
+WARMUP = 10  # 無操作ステップ数
+
+E_START = 1.0  # εの初期値
+E_STOP = 0.01  # εの最終値
+E_DECAY_RATE = 0.001  # εの減衰率
+
+MEMORY_SIZE = 10000  # 経験メモリのサイズ
+BATCH_SIZE = 32  # バッチサイズ
+
+
+def make_model(state_size, action_size):
+    model = Sequential()
+    model.add(Dense(16, activation='relu', input_dim=state_size))
+    model.add(Dense(16, activation='relu'))
+    model.add(Dense(16, activation='relu'))
+    model.add(Dense(action_size, activation='linear'))
+    model.compile(loss=huber_loss, optimizer=Adam(lr=0.001))
+    return model
+
+
+class Memory():
+    # 初期化
+    def __init__(self, memory_size):
+        self.buffer = deque(maxlen=memory_size)
+
+    # 経験の追加
+    def add(self, experience):
+        self.buffer.append(experience)
+
+    # バッチサイズ分の経験をランダムに取得
+    def sample(self, batch_size):
+        idx = np.random.choice(np.arange(len(self.buffer)),
+                               size=batch_size, replace=False)
+        return [self.buffer[i] for i in idx]
+
+    # 経験メモリのサイズ
+    def __len__(self):
+        return len(self.buffer)
+
 
 register()
-
-agent = TsuTreeSearchAgent()
 
 env = make("PuyoPuyoEndlessTsu-v2")
 
 env.reset()
 state = env.get_root()
+# 環境の作成
+state_size = state.width * state.height
+action_size = 22
 
-for i in range(30):
-    action = agent.get_action(state)
-    _, _, done, info = env.step(action)
-    state = info["state"]
-    if done:
+# main-networkの作成
+main_qn = make_model(state_size, action_size)
+
+# target-networkの作成
+target_qn = make_model(state_size, action_size)
+
+# 経験メモリの作成
+memory = Memory(MEMORY_SIZE)
+
+state = np.reshape(state, [1, state_size])
+
+# エピソード数分のエピソードを繰り返す
+total_step = 0  # 総ステップ数
+success_count = 0  # 成功数
+for episode in range(1, NUM_EPISODES + 1):
+    step = 0  # ステップ数
+
+    # target-networkの更新
+    target_qn.set_weights(main_qn.get_weights())
+
+    # 1エピソードのループ
+    for _ in range(1, MAX_STEPS + 1):
+        step += 1
+        total_step += 1
+
+        # εを減らす
+        epsilon = E_STOP + (E_START - E_STOP) * \
+            np.exp(-E_DECAY_RATE * total_step)
+
+        # ランダムな行動を選択
+        if epsilon > np.random.rand():
+            action = state.actions.sample()
+        # 行動価値関数で行動を選択
+        else:
+            action = np.argmax(main_qn.predict(state)[0])
+
+        # 行動に応じて状態と報酬を得る
+        next_state, _, done, info = env.step(action)
+        from IPython import embed
+        embed()
+        next_state = info["state"]
+        env.render()
+        next_state = np.reshape(next_state, [1, state_size])
+
+        _, _, done, info = env.step(action)
+
+        # エピソード完了時
+        if done:
+            # 報酬の指定
+            if step >= MAX_STEPS * 0.9:
+                success_count += 1
+                reward = 1
+            else:
+                success_count = 0
+                reward = 0
+
+            # 次の状態に状態なしを代入
+            next_state = np.zeros(state.shape)
+
+            # 経験の追加
+            if step > WARMUP:
+                memory.add((state, action, reward, next_state))
+        # エピソード完了でない時
+        else:
+            # 報酬の指定
+            reward = 0
+
+            # 経験の追加
+            if step > WARMUP:
+                memory.add((state, action, reward, next_state))
+
+            # 状態に次の状態を代入
+            state = next_state
+
+        # 行動価値関数の更新
+        if len(memory) >= BATCH_SIZE:
+            # ニューラルネットワークの入力と出力の準備
+            inputs = np.zeros((BATCH_SIZE, state_size))  # 入力(状態)
+            targets = np.zeros((BATCH_SIZE, action_size))  # 出力(行動ごとの価値)
+
+            # バッチサイズ分の経験をランダムに取得
+            minibatch = memory.sample(BATCH_SIZE)
+
+            # ニューラルネットワークの入力と出力の生成
+            for i, (state_b, action_b, reward_b,
+                    next_state_b) in enumerate(minibatch):
+
+                # 入力に状態を指定
+                inputs[i] = state_b
+
+                # 採った行動の価値を計算
+                if not (next_state_b == np.zeros(state_b.shape)).all(axis=1):
+                    target = reward_b + GAMMA * \
+                        np.amax(target_qn.predict(next_state_b)[0])
+                else:
+                    target = reward_b
+
+                # 出力に行動ごとの価値を指定
+                targets[i] = main_qn.predict(state_b)
+                targets[i][action_b] = target  # 採った行動の価値
+
+            # 行動価値関数の更新
+            main_qn.fit(inputs, targets, epochs=1, verbose=0)
+
+        # エピソード完了時
+        if done:
+            # エピソードループを抜ける
+            break
+
+    # エピソード完了時のログ表示
+    print('エピソード: {}, ステップ数: {}, epsilon: {:.4f}'.format(episode, step, epsilon))
+
+    # 5回連続成功で学習終了
+    if success_count >= 5:
         break
 
-env.render()
+    # 環境のリセット
+    state = env.reset()
+    state = np.reshape(state, [1, state_size])
