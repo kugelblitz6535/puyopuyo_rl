70a51c82c1dbbd512b844c2c4d573100a7cac521
kugelblitz6535 <user.email=kugelblitz1592@gmail.com>
2019-09-15 18:22:09 +0900

    update: test追加

--- puyopuyo.py
+++ puyopuyo.py
@@ -2,14 +2,14 @@ import numpy as np
 
 
 class PuyoPuyo(object):
-    def __init__(self, width=6, height=10):
+    def __init__(self, width=6, height=11):
         self.width = width
         self.height = height
         self.colors = 4
         self.erase_thresh = 4
-        self.observation_space = ((self.height + 1, self.width), (2,), (2,))
+        self.observation_space = ((self.height, self.width), (2,), (2,))
         self.action_space = self.width * 4 - 2
-        self.field = self.new_field()
+        self.field = self.__new_field()
         self.visited = np.zeros(self.observation_space[0], dtype=np.bool)
         self.erase_map = np.zeros(self.observation_space[0], dtype=np.bool)
         self.next_puyo = self.__get_next_puyo()
@@ -22,13 +22,15 @@ class PuyoPuyo(object):
         actions.pop(-3)
         self.actions = actions
 
-    def new_field(self):
+    def __new_field(self):
         return np.zeros(self.observation_space[0], dtype=np.uint8)
 
     def __get_next_puyo(self):
         return np.random.randint(1, self.colors + 1, size=2)
 
     def legal_actions(self):
+        """選択可能な手の配列を返す。埋まってる列の上には置けない。上まで埋まってる列の向こうには置けない。
+        """
         legal = np.zeros(len(self.actions), dtype=np.bool)
         puttable = self.field[0] == 0
         for i in range(self.action_space):
@@ -52,7 +54,9 @@ class PuyoPuyo(object):
         return np.where(legal)[0]
 
     def reset(self):
-        self.field = self.new_field()
+        """環境をリセットする。
+        """
+        self.field = self.__new_field()
         self.next_puyo = self.__get_next_puyo()
         self.next_next_puyo = self.__get_next_puyo()
         self.score = 0
@@ -60,8 +64,10 @@ class PuyoPuyo(object):
         return self.field, self.next_puyo, self.next_next_puyo
 
     def step(self, action):
-        self.put(action)
-        chain, point = self.chain()
+        """actionで指定した手を行った後の環境、報酬、環境の終了状態、infoを返す。
+        """
+        self.__put(action)
+        chain, point = self.__chain()
         if chain:
             print(f"chain: {chain}")
         self.score += point
@@ -73,7 +79,7 @@ class PuyoPuyo(object):
         return (self.field, self.next_puyo,
                 self.next_next_puyo), chain, self.done, info
 
-    def drop(self, col, puyopuyo):
+    def __drop(self, col, puyopuyo):
         i = np.argmax(np.where(self.field[:, col] == 0))
 
         if isinstance(puyopuyo, np.int64):
@@ -82,24 +88,24 @@ class PuyoPuyo(object):
             self.field[:, col][i] = puyo
             i -= 1
 
-    def put(self, action):
+    def __put(self, action):
         col, rotate = self.actions[action]
         if rotate == 0:
-            self.drop(col, self.next_puyo)
+            self.__drop(col, self.next_puyo)
         elif rotate == 2:
-            self.drop(col, self.next_puyo[::-1])
+            self.__drop(col, self.next_puyo[::-1])
         elif rotate == 1:
-            self.drop(col, self.next_puyo[0])
-            self.drop(col + 1, self.next_puyo[1])
+            self.__drop(col, self.next_puyo[0])
+            self.__drop(col + 1, self.next_puyo[1])
         elif rotate == 3:
-            self.drop(col, self.next_puyo[0])
-            self.drop(col - 1, self.next_puyo[1])
+            self.__drop(col, self.next_puyo[0])
+            self.__drop(col - 1, self.next_puyo[1])
 
-    def out_of_field(self, row, col):
+    def __out_of_field(self, row, col):
         return row < 0 or self.height <= row or col < 0 or self.width <= col
 
-    def check(self, row, col, color):
-        if self.out_of_field(row, col):
+    def __check(self, row, col, color):
+        if self.__out_of_field(row, col):
             return 0
         if self.visited[row][col]:
             return 0
@@ -111,42 +117,40 @@ class PuyoPuyo(object):
         self.visited[row][col] = True
         self.erase_map[row][col] = True
         return 1 + \
-            self.check(row-1, col, color) + \
-            self.check(row, col-1, color) + \
-            self.check(row+1, col, color) + \
-            self.check(row, col+1, color)
+            self.__check(row-1, col, color) + \
+            self.__check(row, col-1, color) + \
+            self.__check(row+1, col, color) + \
+            self.__check(row, col+1, color)
 
-    def fall(self):
+    def __fall(self):
         pass
 
-    def erase(self):
-        self.visited = self.new_field()
+    def __erase(self):
+        self.visited = self.__new_field()
         point = 0
         for row in range(self.height):
             for col in range(self.width):
-                self.erase_map = self.new_field()
-                n = self.check(row, col, self.field[row][col])
+                self.erase_map = self.__new_field()
+                n = self.__check(row, col, self.field[row][col])
                 if self.erase_thresh <= n:
                     point += n * 10
                     self.render()
-                    from IPython import embed
-                    embed()
                     self.field[self.erase_map] = 0
-                    self.fall()
+                    self.__fall()
 
         if 0 < point:
             return True, point
         else:
             return False, 0
 
-    def chain(self):
-        success, got_point = self.erase()
+    def __chain(self):
+        success, got_point = self.__erase()
         chain = 0
         point = 0
         while success:
             chain += 1
             point += got_point * chain
-            success, got_point = self.erase()
+            success, got_point = self.__erase()
 
         return chain, point
 
@@ -156,12 +160,13 @@ class PuyoPuyo(object):
                 if puyo == 0:
                     print('\x1b[0m\u00b7', end="")
                 else:
-                    print(self.int2puyo(puyo), end="")
+                    print(self.__int2puyo(puyo), end="")
                 print(' ', end="")
             print('\x1b[0m')
+        print()
 
     @staticmethod
-    def int2puyo(n):
+    def __int2puyo(n):
         return f"\x1b[3{n}m\u25cf"
 
 
--- tests/test_puyopuyo.py
+++ tests/test_puyopuyo.py
@@ -1,25 +1,78 @@
 import unittest
+
+import numpy as np
+
 from puyopuyo import PuyoPuyo
 
 
 class TestPuyoPuyo(unittest.TestCase):
 
-    def test_new_field(self):
-        self.assertEqual(PuyoPuyo()._PuyoPuyo__new_field().shape, (11, 6))
+    def test_legal_actions(self):
+        env = PuyoPuyo()
+        np.testing.assert_array_equal(env.legal_actions(), np.arange(22))
+        env.field = np.random.randint(1, 5, size=(11, 6))
+        np.testing.assert_array_equal(env.legal_actions(), np.array([]))
+        env.field[0] = 0
+        env.field[1] = 0
+        env.field[0, 1] = 1
+        env.field[[0, 1, 1, 0, 1], [1, 1, 4, 5, 5]] = 1
+        np.testing.assert_array_equal(env.legal_actions(), np.array(
+            [7, 8, 9, 11, 12, 13, 14, 18]))
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_reset(self):
+        env = PuyoPuyo()
+        env.field = np.random.randint(1, 5, size=(11, 6))
+        env.score = 100
+        env.done = True
+        env.reset()
+        np.testing.assert_array_equal(
+            env.field, np.zeros((11, 6), dtype=np.uint8))
+        self.assertEqual(env.score, 0)
+        self.assertFalse(env.done)
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+    def test_step(self):
+        env = PuyoPuyo()
+        field, next_puyo, next_next_puyo = env.reset()
+        np.testing.assert_array_equal(field, np.zeros((11, 6), dtype=np.uint8))
+        (field, new_next_puyo, _), _, done, _ = env.step(8)
+        self.assertFalse(done)
+        self.assertEqual(field[10, 2], next_puyo[0])
+        self.assertEqual(field[10, 3], next_puyo[1])
+        np.testing.assert_array_equal(new_next_puyo, next_next_puyo)
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_step_with_chain(self):
+        env = PuyoPuyo()
+        env.field = np.array(
+            [[0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 1, 0, 0, 0],
+             [0, 0, 1, 0, 0, 0]]
+        )
+        env.next_puyo = np.array([1, 1])
+        (field, _, _), chain, done, _ = env.step(7)
+        np.testing.assert_array_equal(field, np.zeros((11, 6), dtype=np.uint8))
+        self.assertEqual(chain, 1)
+        self.assertFalse(done)
+        # env.field = np.array(
+        #     [[0, 0, 0, 0, 0, 1],
+        #      [0, 0, 0, 0, 1, 1],
+        #      [2, 1, 3, 2, 3, 1],
+        #      [3, 3, 1, 3, 2, 4],
+        #      [2, 3, 1, 1, 4, 3],
+        #      [2, 2, 1, 3, 3, 3],
+        #      [3, 1, 3, 4, 3, 3],
+        #      [1, 1, 2, 3, 4, 4],
+        #      [3, 4, 2, 1, 1, 4],
+        #      [3, 3, 4, 2, 2, 1],
+        #      [4, 4, 2, 1, 1, 4]]
+        # )
 
 
 if __name__ == '__main__':
