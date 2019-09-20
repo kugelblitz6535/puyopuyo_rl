18e756a7a6068cc81feb0367b79966ff72d56c17
kugelblitz6535 <user.email=kugelblitz1592@gmail.com>
2019-09-20 12:35:33 +0900

    update: rewardを変更

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from collections import deque
-
 import keras
 import numpy as np
 from keras.layers import (Activation, BatchNormalization, Conv2D, Dense,
-                          Flatten, Input)
+                          Flatten, Input, MaxPooling2D)
 from keras.models import Model
 from keras.optimizers import Adam
 from tensorflow.losses import huber_loss
@@ -41,20 +39,33 @@ class PuyoPuyoDQN(DQN):
         next_next_puyo = Input(
             shape=state_size[3],
             name='next_next_puyo')
-        x = Conv2D(32, 2)(field)
-        x = Conv2D(32, 2)(x)
+        x = Conv2D(16, 2)(field)
         x = Flatten()(x)
-        x = Dense(128, activation='relu')(x)
-
+        x = Dense(16, activation='relu')(x)
+        x = Dense(16, activation='relu')(x)
         x = keras.layers.concatenate(
             [x, current_puyo, next_puyo, next_next_puyo])
+        x = Dense(16, activation='relu')(x)
+        x = Dense(16, activation='relu')(x)
         output = Dense(action_size, activation='sigmoid', name='output')(x)
         model = Model(inputs=[field, current_puyo, next_puyo, next_next_puyo],
                       output=output)
         model.compile(loss=huber_loss, optimizer=Adam(lr=0.001))
         return model
 
+    def calc_reward(self, step, reward, done):
+        if done:
+            return 0
+        if reward >= 15:
+            return 1
+        else:
+            return 0
+
 
 if __name__ == "__main__":
-    d = PuyoPuyoDQN(PuyoPuyo())
-    d.run(num_episodes=500, max_steps=200, batch_size=8)
+    d = PuyoPuyoDQN(PuyoPuyo(), e_decay_rate=0.0001)
+    d.run(
+        num_episodes=50000,
+        max_steps=200,
+        model_save_episodes=1000,
+        verbose=False)
