b9457fb65118a58012a8207012725a33160631ea
kugelblitz6535 <user.email=kugelblitz1592@gmail.com>
2019-09-26 12:10:09 +0900

    実験版

--- main.py
+++ main.py
@@ -14,22 +14,25 @@ class PuyoPuyoDQN(DQN):
     def state2input(self, state):
         if isinstance(state, tuple):
             field, puyo1, puyo2, puyo3 = state
+            field_input = [field == i + 1 for i in range(4)]
             return [
-                np.expand_dims(np.expand_dims(field, axis=0), axis=3),
+                np.expand_dims(np.stack(field_input, 2), axis=0),
                 np.expand_dims(puyo1, axis=0),
                 np.expand_dims(puyo2, axis=0),
                 np.expand_dims(puyo3, axis=0)
             ]
         else:
+            fields = np.array(
+                [field for (field, _, _, _) in state], dtype=np.uint8)
             return [
-                np.expand_dims(np.array([field for (field, _, _, _) in state], dtype=np.uint8), axis=3),
+                np.array([np.stack([field == i + 1 for i in range(4)], 2) for field in fields]),
                 np.array([puyo for (_, puyo, _, _) in state], dtype=np.uint8),
                 np.array([puyo for (_, _, puyo, _) in state], dtype=np.uint8),
                 np.array([puyo for (_, _, _, puyo) in state], dtype=np.uint8)
             ]
 
     def make_model(self, state_size, action_size):
-        field = Input(shape=(*state_size[0], 1), name='field')
+        field = Input(shape=(*state_size[0], 4), name='field')
         current_puyo = Input(
             shape=state_size[1],
             name='current_puyo')
@@ -39,15 +42,16 @@ class PuyoPuyoDQN(DQN):
         next_next_puyo = Input(
             shape=state_size[3],
             name='next_next_puyo')
+        x = Conv2D(16, 3, activation='relu')(field)
         x = Conv2D(16, 2, activation='relu')(field)
-        x = Conv2D(16, 2, activation='relu')(x)
-        x = Conv2D(16, 2, activation='relu')(x)
         x = Flatten()(x)
-        x = Dense(128, activation='relu')(x)
+        x = Dense(64, activation='relu')(x)
         x = keras.layers.concatenate(
             [x, current_puyo, next_puyo, next_next_puyo])
-        x = Dense(128, activation='relu')(x)
-        output = Dense(action_size, activation='sigmoid', name='output')(x)
+        x = Dense(64, activation='relu')(x)
+        x = Dense(64, activation='relu')(x)
+        x = Dense(64, activation='relu')(x)
+        output = Dense(action_size, activation='linear', name='output')(x)
         model = Model(inputs=[field, current_puyo, next_puyo, next_next_puyo],
                       output=output)
         model.compile(loss=huber_loss, optimizer=Adam(lr=0.001))
@@ -56,14 +60,15 @@ class PuyoPuyoDQN(DQN):
     def calc_reward(self, step, reward, done):
         if done:
             return -1
-        if reward >= 15:
-            return 1
-        else:
-            return 0
+        return reward / 15
+        # if reward >= 15:
+        #     return 1
+        # else:
+        #     return 0
 
 
 if __name__ == "__main__":
-    d = PuyoPuyoDQN(PuyoPuyo(), e_decay_rate=0.0001)
+    d = PuyoPuyoDQN(PuyoPuyo(), e_decay_rate=0.00001, memory_length=1000)
     d.run(
         num_episodes=50000,
         max_steps=200,
